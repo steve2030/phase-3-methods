3c171dea3829bc1d8851d3ede7179d520ab7c88b
steve <stevemighty2030@gmail.com>
2022-10-17 23:06:05 +0300

    passed the test successfully

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+# here is the first method
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+
+# here is the second method
+def greet(programmer)
+  puts "Hello, #{programmer}!"
+end
+
+
+# here is the third method
+def greet_with_default(name="programmer")
+  puts "Hello, #{name}!"
+end
+
+
+
+
+# here is the fourth methods
+def add(numb1,numb2)
+   numb1+numb2
+end
+
+# here is the fifth function
+def halve (value)
+  if value.class != Integer
+    return nil
+  end
+  (value/2)
+end
+halve(8)
